5ab935050666ab1bf6e02982343b5893dc398b82
kapil-goel952 <goelk849@gmail.com>
2026-06-13 00:00:25 +0530

    initial commit

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-
+import "tailwindcss";
 const App = () => {
   return (
-    <div>
-      <h1>hellllllo   </h1>
+    <div className='bg-[#2d3441] h-50 flex justify-between text-white w-full pl-20 pr-20    ' >
+      <div className='flex mt-20 '>
+        <img src="logo.png" alt="logo" />
+        <h1>Portfolio</h1>
+      </div>
+
+      <div className=' flex mt-15 pt-3 gap-x-10 '>
+         <div className=' bg-black flex'>
+          <p className='text-[purple]'>#</p>
+          <button >Home</button>
+        </div>
+         <div className=' bg-black flex'>
+          <p className='text-[purple]'>#</p>
+          <button >Works</button>
+        </div>
+         <div className=' bg-black flex'>
+          <p className='text-[purple]'>#</p>
+          <button >About me</button>
+        </div>
+         <div className=' bg-black flex'>
+          <p className='text-[purple]'>#</p>
+          <button >Contact</button>
+        </div>
+       
+
+
+      </div>
+
     </div>
   )
 }
