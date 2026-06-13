b6171d8db367bcfbb70456faa9043a2c327ce70f
kapil-goel952 <goelk849@gmail.com>
2026-06-14 01:16:45 +0530

    completed hero section

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import Navbar from './Components/navbar'
 import HeroSection from './Components/heroSection'
-import Hero from './assets/hero.svg'
+
 
 
 const App = () => {
   return (
-    <div className='pl-30 pr-30 font-serif h-full w-s bg-[#282c33]'>
+    <div className='pl-30 pr-30 font-serif h-870 w-s bg-[#282c33]'>
       <Navbar />
-      <HeroSection/>
+      <HeroSection />
+      <div className="projects h-150 bg-amber-950 w-full  font-mono  px-20 mb-10">
+        <p className='text-white text-4xl font-black'><span className='text-purple-400 text-4xl'>#</span>Projects</p>
+      </div>
+      <div className="skills h-150 bg-amber-950 w-full     font-mono px-20 mb-10">
+        <p className='text-white text-4xl font-black'><span className='text-purple-400 text-4xl'>#</span>Skills</p>
+      </div>
+      <div className="about me h-150 bg-amber-950 w-full font-mono   px-20 mb-10">
+        <p className='text-white text-4xl font-black'><span className='text-purple-400 text-4xl'>#</span>About me</p>
+      </div>
+      <div className="contact h-150 bg-amber-950 w-full font-mono   px-20 mb-10">
+        <p className='text-white text-4xl font-black'><span className='text-purple-400 text-4xl'>#</span>Contact</p>
+      </div>
+      <div className="footer  h-50 bg-amber-900 w-full px-20 mt-10">
+
+      </div>
     </div>
   )
 }
