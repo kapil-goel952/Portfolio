09c62bfc32ee9c922579ae195df27ecd9e4a9e60
kapil-goel952 <goelk849@gmail.com>
2026-06-16 23:57:52 +0530

    projects section completed

---
+++ Portfolio/src/Components/home/LetsBuild.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const LetsBuild = () => {
+  return (
+   <section className="w-full mt-16 px-4 sm:px-8 lg:px-20 mb-20">
+
+        {/* Heading */}
+        <p className="text-white text-3xl sm:text-4xl font-black mb-8">
+          <span className="text-purple-500">#</span>
+          Let's Buuild something Amazing
+        </p>
+
+
+        <div className="border border-gray-700 rounded-xl p-6 lg:p-8 bg-[#2c3036]">
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-8">
+
+            <div className="flex items-center gap-5">
+              <img
+                src="https://img.icons8.com/?size=100&id=7OTzx1U7zZJE&format=png&color=7950F2"
+                alt="mail"
+                className="h-12 w-12 sm:h-16 sm:w-16"
+              />
+              <p className="text-white text-lg sm:text-xl lg:text-2xl font-semibold leading-relaxed">
+                Have a project in mind
+                <br />
+                or just want to say hi?
+              </p>
+            </div>
+
+            <div className="max-w-md">
+              <p className="text-gray-400 text-base sm:text-lg leading-relaxed">
+                I'm always open to discussing new ideas,
+                collaborations, creative projects and
+                exciting opportunities.
+              </p>
+            </div>
+
+            <div>
+              <Link to="/Contact" className="inline-block bg-linear-to-r from-purple-500 to-purple-700 px-6 py-3 rounded-lg text-white font-semibold hover:scale-105 transition-all duration-300">
+                Get in Touch →
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+
+  )
+}
+
+export default LetsBuild
--- Portfolio/src/pages/home.jsx
+++ Portfolio/src/pages/home.jsx
@@ -4,6 +4,7 @@ import Herosection from '../Components/home/heroSection'
 import ProjectCard from '../Components/home/ProjectCard'
 import { Link } from 'react-router-dom'
 import Projects from '../Components/home/Projects'
+import LetsBuild from '../Components/home/LetsBuild'
 
 const Home = () => {
   return (
@@ -46,48 +47,7 @@ const Home = () => {
       </section>
 
       {/* Contact Section */}
-      <section className="w-full mt-16 px-4 sm:px-8 lg:px-20 mb-20">
-
-        {/* Heading */}
-        <h2 className="text-white text-2xl sm:text-4xl lg:text-5xl  mb-10">
-          <span className="text-purple-500">#</span>
-          LET'S BUILD SOMETHING AMAZING
-        </h2>
-
-        {/* Content Card */}
-        <div className="border border-gray-700 rounded-xl p-6 lg:p-8 bg-[#2c3036]">
-          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-8">
-            {/* Left */}
-            <div className="flex items-center gap-5">
-              <img
-                src="https://img.icons8.com/?size=100&id=7OTzx1U7zZJE&format=png&color=7950F2"
-                alt="mail"
-                className="h-12 w-12 sm:h-16 sm:w-16"
-              />
-              <p className="text-white text-lg sm:text-xl lg:text-2xl font-semibold leading-relaxed">
-                Have a project in mind
-                <br />
-                or just want to say hi?
-              </p>
-            </div>
-            {/* Middle */}
-            <div className="max-w-md">
-              <p className="text-gray-400 text-base sm:text-lg leading-relaxed">
-                I'm always open to discussing new ideas,
-                collaborations, creative projects and
-                exciting opportunities.
-              </p>
-            </div>
-            {/* Right */}
-            <div>
-              <Link to="/Contact" className="inline-block bg-linear-to-r from-purple-500 to-purple-700 px-6 py-3 rounded-lg text-white font-semibold hover:scale-105 transition-all duration-300">
-                Get in Touch →
-              </Link>
-            </div>
-          </div>
-        </div>
-      </section>
-
+      <LetsBuild/>
     </div>
   )
 }
