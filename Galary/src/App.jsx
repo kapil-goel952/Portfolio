e16214af5a7c7a5016172cb7c87b33ce24be105f
kapil-goel952 <goelk849@gmail.com>
2026-06-22 23:24:18 +0530

    1st commit of project

--- Galary/src/App.jsx
+++ Galary/src/App.jsx
@@ -2,32 +2,20 @@ import { useState } from 'react'
 import axios from 'axios'
 
 function App() {
-//   const [data,setdata]=useState([])
-//  async function getData(){
-//     const response= await axios.get('https://picsum.photos/v2/list')
-//       setdata(response.data)
-//       console.log([response.data.author]);
-      
-      // }
- 
+  cosnt [response,setresponse]=useState([])
+  function getdata(){
+    const data = fetch('https://picsum.photos/v2/list')
+    console.log(data.data);
+    
+  }
   return (
    <div className='h-100 w-100 bg-red-200'>
       <div>
         <h1>hello guyss</h1>
       </div>
-      {/* <div>
-        {data.map((elem,idx)=>{
-          return (
-            <div key={elem.id}>
-                <h1> ram ram {idx} </h1>
-                <p>{elem.author}</p>
-            </div>
-          )
-        })}
-      </div> */}
       <div>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ex et, velit quos illum hic voluptas delectus natus, dolor rem sit libero laudantium repellendus dolores. Laboriosam beatae accusamus impedit veritatis!</p>
-        <button onClick={getData}
+        <button onClick={()=>{getdata()}}
         className='bg-green-300'>
           get data
         </button>
@@ -37,3 +25,21 @@ function App() {
 }
 
 export default App
+
+{/* <div>
+  {data.map((elem,idx)=>{
+    return (
+      <div key={elem.id}>
+          <h1> ram ram {idx} </h1>
+          <p>{elem.author}</p>
+      </div>
+    )
+  })}
+</div> */}
+    //   const [data,setdata]=useState([])
+    //  async function getData(){
+    //     const response= await axios.get('https://picsum.photos/v2/list')
+    //       setdata(response.data)
+    //       console.log([response.data.author]);
+          
+          // }
