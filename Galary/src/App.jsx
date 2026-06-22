9322a35e673b027ca5522ea3bf753d275971eaa1
kapil-goel952 <goelk849@gmail.com>
2026-06-23 02:40:17 +0530

    completed the designing and buttons functionality

--- Galary/src/App.jsx
+++ Galary/src/App.jsx
@@ -2,56 +2,82 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function App() {
-  const [data,setdata]=useState([])
- async  function getdata(){
-    const response =await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
+  const [index, setindex] = useState(1)
+  const [data, setdata] = useState([])
+  async function getdata() {
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=100`)
     console.log(response.data);
     setdata(response.data)
   }
 
-  let userdata=<h1 className='text-white text-2xl'>no user avaliable</h1>
-   if (data.length>0) {
-    userdata=data.map((elem,idx)=>{
-            return(
-              <div key={idx} className='h-50 w-50 border-2  rounded-2xl'>
-                <img src={elem.download_url} alt="img" className='h-full w-full' />
-              </div>
-            )
-        })
-   }
+  let userdata = <h1 className='text-white text-2xl'>no user avaliable</h1>
+  if (data.length > 0) {
+    userdata = data.map((elem, idx) => {
+      return (
+        <div key={elem.id} className='bg-gray-800 rounded-2xl p-[0.6vw]'>
+          <div className='h-[25vh] border-2'>
+            <img src={elem.download_url} alt="img" className='h-full w-full object-cover' />
+          </div>
+          <div>
+            <h2 className='font-black text-xl text-white '>
+              {elem.author}
+            </h2>
+          </div>
+        </div>
+      )
+    })
+  }
 
-   useEffect(
-()=>{
-  getdata()
-}
-   ,[])
+  useEffect(
+    () => {
+      getdata()
+    }
+    , [index])
   return (
-   <div className='h-full w-screen min-h-screen flex flex-col  bg-gray-900'>
-   
-    <div className='flex flex-wrap gap-4'>
-      {userdata}
+    <div className='h-full w-screen min-h-screen flex flex-col px-[2vw]  bg-gray-900'>
+      <div className="header  fixed  top-0  left-[2vw]  right-[2vw]  flex  justify-between  items-center  h-[9vh]  px-6  bg-gray-800  rounded-b-2xl">
+        <h1 className='font-black text-4xl text-white '>Gallary</h1>
+        <form>
+          <input type="text" placeholder='🔍 search your image' className='text-white w-[20vw] border-2 border-gray-500 px-5 rounded' />
+        </form>
+      </div>
+      <div className='flex pt-[12vh] flex-wrap gap-4'>
+        {userdata}
+      </div>
+      <div className='flex items-center justify-center'>
+        <button
+          className='text-2xl font-black text-black bg-amber-400 m-3 px-4 rounded py-2 hover:bg-yellow-600 transition-all duration-300'
+          onClick={() => {
+            if (index > 1) {
+              setindex(index - 1);
+              window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+              })
+            }
+          }}
+        >
+          prev
+        </button>
+        <p className="text-white text-xl font-bold">
+          Page {index}
+        </p>
+
+        <button
+          className='text-2xl font-black text-black bg-amber-400 m-3 px-4 rounded py-2 hover:bg-yellow-600 transition-all duration-300'
+          onClick={() => {
+            setindex(index + 1);
+            window.scrollTo({
+              top: 0,
+              behavior: "smooth"
+            })
+          }}
+        >
+          next
+        </button>
+      </div>
     </div>
-   </div>
   )
 }
 
 export default App
-
- 
-{/* <div>
-  {data.map((elem,idx)=>{
-    return (
-      <div key={elem.id}>
-          <h1> ram ram {idx} </h1>
-          <p>{elem.author}</p>
-      </div>
-    )
-  })}
-</div> */}
-    //   const [data,setdata]=useState([])
-    //  async function getData(){
-    //     const response= await axios.get('https://picsum.photos/v2/list')
-    //       setdata(response.data)
-    //       console.log([response.data.author]);
-          
-          // }
